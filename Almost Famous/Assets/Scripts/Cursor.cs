3788a8c73dbca4f03039c870cfe6182fcb3a1a36
dwilsonaieyr1 <JittNA@aol.com>
2016-02-15 16:59:19 -0600

    a
    a

---
+++ Almost Famous/Assets/Scripts/Cursor.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cursor : MonoBehaviour
+{
+
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x - 8, Input.mousePosition.y + 11, +10));
+        if (Input.GetMouseButtonDown(0) == true)
+        {
+            GetComponent<AudioSource>().Play();
+        }
+
+
+
+
+
+
+    }
+}
+
+
